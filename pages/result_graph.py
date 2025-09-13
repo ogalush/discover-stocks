8b4b8548f6ca1a87e04496864fe3db0c86b91420
Takehiko OGASAWARA <take_oga@hotmail.com>
2025-09-14 01:40:40 +0900

    日本株, 米国株のグラフを plotly_chart へ変更

--- pages/result_graph.py
+++ pages/result_graph.py
@@ -2,6 +2,8 @@
 import streamlit as st
 import datetime
 import re
+import plotly.express as px
+import plotly.graph_objects as go
 from utils.db import get_connection
 
 # 取得・表示の最大日数 (DB負荷考慮)
@@ -81,7 +83,7 @@ def show(selected_date):
 
             # DataFrameに変換
             def convert_to_df(vote_results):
-                df = pd.DataFrame(vote_results, columns=["日付", "銘柄コード", "投票数"])
+                df = pd.DataFrame(vote_results, columns=["日付", "銘柄", "投票数"])
                 df["日付"] = pd.to_datetime(df["日付"])  # 日付をDatetime型に変換
                 return df
 
@@ -103,22 +105,61 @@ def show(selected_date):
                 {"result_key": "米国株", "result_value": results_us}
             ]
 
+            # 投票推移を線で結んだ折線グラフを準備する
             df = None
-            for result in result_list:
-                st.subheader(result["result_key"])
-                df = convert_to_df(result["result_value"])
-                filtered_df = df[(df["日付"] >= pd.to_datetime(start_date)) & (df["日付"] <= pd.to_datetime(end_date))].copy()
-                filtered_df["日付"] = filtered_df["日付"].dt.strftime("%m月%d日") # 日付の表示形式変換
+            for label, result in [("日本株", results_jp), ("米国株", results_us)]:
+                st.subheader(label)
+                df = convert_to_df(result)
+                filtered_df = df[(df["日付"] >= pd.to_datetime(start_date)) & (df["日付"] <= pd.to_datetime(end_date))]
+                filtered_df = filtered_df.sort_values("日付")
 
-                options = st.multiselect("銘柄コードを選択してください:", sorted(df["銘柄コード"].unique().tolist()), default=[])
+                # マルチセレクトで銘柄選択
+                options = st.multiselect("銘柄を選択してください:", sorted(filtered_df["銘柄"].unique().tolist()), default=[])
+
+                # 銘柄コードがない日付に NaN（欠損値）を入れる対応
+                df_pivot = filtered_df.pivot(index="日付", columns="銘柄", values="投票数")
+                df_selected = df_pivot.reindex()  # 存在しない場合は NaN になる
                 if options:
-                     # 銘柄コードがない日付に NaN（欠損値）を入れる対応
-                     df_pivot = filtered_df.pivot(index="日付", columns="銘柄コード", values="投票数")
-                     df_selected = df_pivot.reindex(columns=options)  # 存在しない場合は NaN になる
-                     st.line_chart(df_selected, use_container_width=True)
+                    df_selected = df_pivot.reindex(columns=options) # 存在しない場合は NaN になる
 
-                else:
-                    st.line_chart(filtered_df, x="日付", y="投票数", color="銘柄コード", use_container_width=True)
+                # 日付毎に描画する
+                fig = go.Figure()
+                df_selected = df_selected.sort_values(by="日付")
+                for stock_code in df_selected.columns:
+                    fig.add_trace(go.Scatter(
+                        x=df_selected.index,
+                        y=df_selected[stock_code],
+                        mode='lines+markers',
+                        name=stock_code,
+                        customdata=[[stock_code]] * len(df_selected),  # 銘柄名を customdata として渡す
+                        hovertemplate='<extra>◆</extra>%{x|%Y-%m-%d} %{y} 票<br>%{customdata[0]}',
+                        connectgaps=True  # 欠損があっても線を繋げる
+                    ))
+
+                # 標準だとフォントが小さいので少しだけ大きくする
+                fontsize=dict(size=13)
+
+                # X軸の形式を "YYYY-MM-DD" に設定
+                fig.update_xaxes(tickformat="%Y-%m-%d", tickfont=fontsize)
+
+                # Y軸の最小値を0に設定
+                fig.update_yaxes(range=[0, df["投票数"].max()], tickfont=fontsize)
+
+                fig.update_layout(
+                    xaxis=dict(fixedrange=False),                   # スケール変更を許可
+                    yaxis=dict(range=[0, None], fixedrange=False),  # Y軸最小値を0にしつつ、スケール変更を許可
+                    xaxis_title="日付",
+                    yaxis_title="投票数",
+                    legend=dict(font=fontsize),     # 凡例のフォントサイズ
+                    hoverlabel=dict(font=fontsize), # グラフの値のフォントサイズ
+                    template="plotly_dark",
+                    hovermode="closest",            # マウスオーバー時の凡例をその銘柄だけに絞る
+                    dragmode="pan",                 # ドラッグ時に画面移動させる
+                    width=1500,                     # グラフ本体(幅)
+                    height=600,                     # グラフ本体(高さ)
+                    showlegend=False                # 凡例を非表示化
+                )
+                st.plotly_chart(fig, use_container_width=True, config={"scrollZoom": True, "displayModeBar": True, "staticPlot": False})
 
             # 投票数の合計と投票ボタンが押された回数の表示 (#13の追従)
             # SQLの結果をDataFrameに変換
